14ff896ea01dedd01516bbd284f03dbab5296822
Divine-kuzo <d.kuzo@alustudent.com>
2025-03-26 03:10:18 -0700

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-""""Function documentation"""
+"""Defines an  an integer addition function."""
 
 
-import unittest
-
-max_integer = __import__('6-max_integer').max_integer
-
-
-class TestMaxInteger(unittest.TestCase):
-    """"Function documentation"""
-
-    def test_simple_case(self):
-        """"Function documentation"""
-        self.assertEqual(max_integer([1, 2, 3]), 3)
-
-    def test_negative_case(self):
-        """"Function documentation"""
-        self.assertEqual(max_integer([-1, -2, -3]), -1)
-
-    def test_empty_case(self):
-        """"Function documentation"""
-        self.assertEqual(max_integer([]), None)
-
-    def test_max_middle_case(self):
-        """"Function documentation"""
-        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
-
-    def test_one_element_case(self):
-        """"Function documentation"""
-        self.assertEqual(max_integer([5]), 5)
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
+    Second integer has a default value of 98 if value is not specified
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
